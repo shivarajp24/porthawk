f908557058a9bed163e8bf4aabe0d84b81324804
Shivaraj Patil <tryhackme2408@gmail.com>
2026-09-06 02:28:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="porthawk",
-    version="2.0.2",
+    version="2.0.3",
     description="Fast multi-threaded port scanner - Nmap alternative",
     long_description=long_description,
     long_description_content_type="text/markdown",
